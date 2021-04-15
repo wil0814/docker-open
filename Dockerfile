878734a10ce2fc6da935563f8971814f518437a9
william <william880814123@gmail.com>
2021-04-15 16:13:05 +0800

    william:test images 修改

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM centos:8
 RUN yum upgrade -y \
 &&  yum install epel-release -y \
 &&  yum update -y \
-&&  yum install nginx -y
+&&  yum install httpd httpd-tools -y
 
 
-EXPOSE 8081
+EXPOSE 8082
 
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["/usr/sbin/httpd","-D","FOREGROUND"]
--- select.php
+++ select.php
@@ -6,7 +6,7 @@ $iso = $_POST["iso"];
 $server = $_POST["server"];
 //新增系統
 if($iso =="youbike"){
-$output = shell_exec('sudo docker run --rm -p8080:80 -p8081:80 -p3306:3306 -itd william880814123/0107-test');
+$output = shell_exec('sudo docker run --rm -p8080:80  -itd william:test');
 echo $output;
 }
 
